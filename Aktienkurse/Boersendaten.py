1da7b95b2e5a8a211e7d7a8823fe6765e0903d5e
pi <sebastian.a.kaspar@gmail.com>
2015-10-11 20:30:22 +0200

    Knockout added

--- Aktienkurse/Boersendaten.py
+++ Aktienkurse/Boersendaten.py
@@ -23,8 +23,9 @@ print "Downloading current stocks..."
 for key in sorted(shares):
     url = urllib2.urlopen(shares[key]["url"])
     with open("/var/tmp"+key+".txt", 'w') as f:
-        i=0    
+        linenumber=0    
         for line in url:
+            linenumber +=1
             if line.find("itemprop="+"\""+"price")>=0:
                 try:
                     start = str(line).index("itemprop="+"\""+"price")+17
@@ -40,20 +41,38 @@ for key in sorted(shares):
                         pm = "+"
                     percentChange = np.abs((aktuellerKurs-shares[key]["Kaufkurs"])/shares[key]["Kaufkurs"])  
                     if 0.15 < percentChange:
-                        message = "> "+str(key), aktuellerKurs, str(pm)+str(change)                     
+                        message = "> "+str(key)+": "+str(aktuellerKurs)+", " + str(pm)+str(change)                     
                     else:
-                        message = "  "+str(key), aktuellerKurs, str(pm)+str(change)
+                        message = "  "+str(key)+": " + str(aktuellerKurs)+", " + str(pm)+str(change)
                     pushMessageString+=str(message)
-                    pushMessageString+="\n"
                 except:
                     print "Price not found! Check script."
+            if key in ["CHFhighRisk", "CHFlowRisk", "DaxKO"]:                
+                if line.find("Einfacher Hebel</td")>=0:
+                    try:
+                        start = str(line).index("Einfacher Hebel</td> <td class")+38
+                        end = str(line).index(" </td> </tr> <tr> <td class=")-1
+                        hebel = round(float(line[start:end].replace(",",".")),2)
+                        pushMessageString+=", ["+str(hebel)
+                    except:
+                        print "Error reading Hebel"
+                if line.find("Einfacher Hebel</td")>=0:
+                    try:
+                        start = str(line).index("Einfacher Hebel</td> <td class")+38
+                        end = str(line).index(" </td> </tr> <tr> <td class=")-1
+                        knockout = round(float(line[start:end].replace(",",".")),2)
+                        pushMessageString+=", "+str(knockout)+"]"
+                    except:
+                        print "Error reading Knock-Out"
+ 
             f.write(line)
+    pushMessageString+="\n"
 print pushMessageString
 
 if os.path.isfile("/home/pi/PycharmProjects/PushNotification/myapikey"):
         apikey = open("/home/pi/PycharmProjects/PushNotification/myapikey",'r').readline().strip()
 p = pynma.PyNMA(apikey)
 p.push("System: Pi", "Event: Boersenmonitor", str(pushMessageString))
-print "Push notificarion sent"
+print "Push notification sent"
 
 
