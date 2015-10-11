293ed1fe38ccd4313ac9617397c48e208fc91bf5
pi <sebastian.a.kaspar@gmail.com>
2015-10-11 19:18:29 +0200

    Aktienkurs als push notification auf android

--- Aktienkurse/Boersendaten.py
+++ Aktienkurse/Boersendaten.py
@@ -7,17 +7,22 @@ Created on Sat Oct 10 15:48:59 2015
 import urllib2
 import sys
 import json
+import numpy as np
+import pynma
+import os
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+pushMessageString = ""
 shares = {}
 with open("/home/pi/PycharmProjects/Aktienkurse/myShares.txt") as jsondict:
     shares = json.load(jsondict)
 
+print "Downloading current stocks..."
 for key in sorted(shares):
     url = urllib2.urlopen(shares[key]["url"])
-    with open("/home/pi/PycharmProjects/Aktienkurse/Daten/"+key+".txt", 'w') as f:
+    with open("/var/tmp"+key+".txt", 'w') as f:
         i=0    
         for line in url:
             if line.find("itemprop="+"\""+"price")>=0:
@@ -27,8 +32,28 @@ for key in sorted(shares):
                         end = str(line).index("&nbsp;&euro;<")
                     else:
                         end = str(line).index("</span")
-                    print key, line[start:end]
+                    aktuellerKurs = round(float(line[start:end].replace(",",".")),2)
+                    change = round((aktuellerKurs - shares[key]["Kaufkurs"])*shares[key]["Anteile"],2)
+                    if change < 0:
+                        pm = ""
+                    else:
+                        pm = "+"
+                    percentChange = np.abs((aktuellerKurs-shares[key]["Kaufkurs"])/shares[key]["Kaufkurs"])  
+                    if 0.15 < percentChange:
+                        message = "> "+str(key), aktuellerKurs, str(pm)+str(change)                     
+                    else:
+                        message = "  "+str(key), aktuellerKurs, str(pm)+str(change)
+                    pushMessageString+=str(message)
+                    pushMessageString+="\n"
                 except:
                     print "Price not found! Check script."
             f.write(line)
+print pushMessageString
+
+if os.path.isfile("/home/pi/PycharmProjects/PushNotification/myapikey"):
+        apikey = open("/home/pi/PycharmProjects/PushNotification/myapikey",'r').readline().strip()
+p = pynma.PyNMA(apikey)
+p.push("System: Pi", "Event: Boersenmonitor", str(pushMessageString))
+print "Push notificarion sent"
+
 
