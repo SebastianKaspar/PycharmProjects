0235250dd2e194fb17ddfd00e7ce27e904ef06d1
Sebastian Kaspar <sebastian.a.kaspar@gmail.com>
2015-10-12 22:29:26 +0200

    Relative paths

--- Aktienkurse/Boersendaten.py
+++ Aktienkurse/Boersendaten.py
@@ -14,52 +14,59 @@ import os
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+
+def get_path():
+    path = os.path.dirname(os.path.abspath(__file__))
+    return path
+
+
 pushMessageString = ""
 shares = {}
-with open("/home/pi/PycharmProjects/Aktienkurse/myShares.txt") as jsondict:
+mainpath = get_path()
+with open(os.path.join(mainpath, "myShares.txt")) as jsondict:
     shares = json.load(jsondict)
 
 print "Downloading current stocks..."
 for key in sorted(shares):
     url = urllib2.urlopen(shares[key]["url"])
-    with open("/var/tmp"+key+".txt", 'w') as f:
+    with open(os.path.join("/var/tmp", key + ".txt"), 'w') as f:
         for line in url:
-            if line.find("itemprop="+"\""+"price")>=0:
+            if line.find("itemprop=" + "\"" + "price") >= 0:
                 try:
-                    start = str(line).index("itemprop="+"\""+"price")+17
+                    start = str(line).index("itemprop=" + "\"" + "price") + 17
                     if key not in ["CHFzuEUR"]:
                         end = str(line).index("&nbsp;&euro;<")
                     else:
                         end = str(line).index("</span")
-                    aktuellerKurs = round(float(line[start:end].replace(",",".")),2)
-                    change = round((aktuellerKurs - shares[key]["Kaufkurs"])*shares[key]["Anteile"],2)
+                    aktuellerKurs = round(float(line[start:end].replace(",", ".")), 2)
+                    change = round((aktuellerKurs - shares[key]["Kaufkurs"]) * shares[key]["Anteile"], 2)
                     if change < 0:
                         pm = ""
                     else:
                         pm = "+"
-                    percentChange = np.abs((aktuellerKurs-shares[key]["Kaufkurs"])/shares[key]["Kaufkurs"])  
+                    percentChange = np.abs((aktuellerKurs - shares[key]["Kaufkurs"]) / shares[key]["Kaufkurs"])
                     if 0.15 < percentChange:
-                        message = "> "+str(key)+": "+str(aktuellerKurs)+", " + str(pm)+str(change)                     
+                        message = "> " + str(key) + ": " + str(aktuellerKurs) + ", " + str(pm) + str(change)
                     else:
-                        message = "  "+str(key)+": " + str(aktuellerKurs)+", " + str(pm)+str(change)
-                    pushMessageString+=str(message)
+                        message = "  " + str(key) + ": " + str(aktuellerKurs) + ", " + str(pm) + str(change)
+                    pushMessageString += str(message)
                 except:
                     print "Price not found! Check script."
-            if key in ["CHFhighRisk", "CHFlowRisk", "DaxKO"]:                
-                if line.find("Einfacher Hebel</td")>=0:
+            if key in ["CHFhighRisk", "CHFlowRisk", "DaxKO"]:
+                if line.find("Einfacher Hebel</td") >= 0:
                     try:
-                        start = str(line).index("Einfacher Hebel</td> <td class")+38
-                        end = str(line).index(" </td> </tr> <tr> <td class=")-1
-                        hebel = round(float(line[start:end].replace(",",".")),2)
-                        pushMessageString+=", ["+str(hebel)+"]"
+                        start = str(line).index("Einfacher Hebel</td> <td class") + 38
+                        end = str(line).index(" </td> </tr> <tr> <td class=") - 1
+                        hebel = round(float(line[start:end].replace(",", ".")), 2)
+                        pushMessageString += ", [" + str(hebel) + "]"
                     except:
                         print "Error reading Hebel"
             f.write(line)
-    pushMessageString+="\n"
+    pushMessageString += "\n"
 print pushMessageString
 
-if os.path.isfile("/home/pi/PycharmProjects/PushNotification/myapikey"):
-        apikey = open("/home/pi/PycharmProjects/PushNotification/myapikey",'r').readline().strip()
+if os.path.isfile(os.path.join(mainpath, "myapikey")):
+    apikey = open(os.path.join(mainpath, "myapikey"), 'r').readline().strip()
 p = pynma.PyNMA(apikey)
 p.push("System: Pi", "Event: Boersenmonitor", str(pushMessageString))
 print "Push notification sent"
